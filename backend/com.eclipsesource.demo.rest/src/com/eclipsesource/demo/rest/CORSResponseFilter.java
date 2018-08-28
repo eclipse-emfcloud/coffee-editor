43626a0915c6ad00e86410f3446c0a6e42bfe279
Eugen Neufeld <eneufeld@eclipsesource.com>
2018-08-28 21:27:13 +0200

    add rest server

---
+++ backend/com.eclipsesource.demo.rest/src/com/eclipsesource/demo/rest/CORSResponseFilter.java
@@ -0,0 +1,27 @@
+package com.eclipsesource.demo.rest;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.Provider;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component
+@Provider
+public class CORSResponseFilter implements ContainerResponseFilter {
+
+	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+			throws IOException {
+
+		MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+
+		headers.add("Access-Control-Allow-Origin", "*");
+		headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");			
+		headers.add("Access-Control-Allow-Headers", "Content-Type");
+	}
+
+}
