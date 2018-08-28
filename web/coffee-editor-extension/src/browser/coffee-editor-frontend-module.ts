bfa737f273a2bfd5913305a3c0bd620254f1907c
Edgar Mueller <emueller@eclipsesource.com>
2018-08-28 09:55:36 +0200

    Provide save/load hooks
    * Add required bindings after theia-tree-module has been removed
    * Switch to experimental branch of theia-tree-editor

--- web/coffee-editor-extension/src/browser/coffee-editor-frontend-module.ts
+++ web/coffee-editor-extension/src/browser/coffee-editor-frontend-module.ts
@@ -4,14 +4,27 @@
 
 import { ContainerModule } from "inversify";
 import {
+  CommandContribution, MenuContribution, Resource,
   ResourceProvider
 } from "@theia/core/lib/common";
-import { TreeEditorWidget, TreeEditorWidgetOptions } from 'theia-tree-editor/lib/browser';
-import { WidgetFactory } from '@theia/core/lib/browser';
+import { TheiaTreeEditorContribution, ResourceSaveable, TreeEditorWidget, TreeEditorWidgetOptions } from 'theia-tree-editor/lib/browser';
+import {OpenHandler, WidgetFactory} from '@theia/core/lib/browser';
 import URI from '@theia/core/lib/common/uri';
 import { CoffeeApp, initStore } from './coffee-editor';
 
 import '../../src/browser/style/index.css';
+import {getData} from "@jsonforms/core";
+
+class MyResourceSaveable extends ResourceSaveable {
+  constructor(resource: Resource, getData: () => any) {
+    super(resource, getData);
+  }
+
+  onSave(data: any) {
+    console.log("I was called during save");
+    return super.onSave(data);
+  }
+}
 
 export default new ContainerModule(bind => {
   bind<WidgetFactory>(WidgetFactory).toDynamicValue(ctx => ({
@@ -22,8 +35,24 @@ export default new ContainerModule(bind => {
       const store = await initStore();
       const child = container.createChild();
       child.bind<TreeEditorWidgetOptions>(TreeEditorWidgetOptions)
-        .toConstantValue({ resource, store, EditorComponent: CoffeeApp, fileName: new URI(uri).path.base});
+        .toConstantValue({
+          resource,
+          store,
+          EditorComponent: CoffeeApp,
+          fileName: new URI(uri).path.base,
+          saveable: new MyResourceSaveable(resource, () => getData(store.getState())),
+          onResourceLoad: contentAsString => {
+            console.log("I was called during resource load");
+            return JSON.parse(contentAsString)
+          }
+        });
       return child.get(TreeEditorWidget);
     }
   }));
+  bind(TreeEditorWidget).toSelf();
+
+  bind(TheiaTreeEditorContribution).toSelf().inSingletonScope();
+  [CommandContribution, MenuContribution, OpenHandler].forEach(serviceIdentifier =>
+    bind(serviceIdentifier).toService(TheiaTreeEditorContribution)
+  );
 });
