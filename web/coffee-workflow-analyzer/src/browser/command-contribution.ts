0c345bc0ed18108e4731bb355452c1f5f07701ea
Alexandra Buzila <abuzila@eclipsesource.com>
2018-08-29 20:09:41 +0200

    Analyze command - load wf and wfconfig from filesystem

--- web/coffee-workflow-analyzer/src/browser/command-contribution.ts
+++ web/coffee-workflow-analyzer/src/browser/command-contribution.ts
@@ -9,6 +9,7 @@ import URI from "@theia/core/lib/common/uri";
 import { MiniBrowserOpenHandler } from "@theia/mini-browser/lib/browser/mini-browser-open-handler";
 import { FileTypes } from "coffee-workflow-analyzer-editors/lib/node/file-server";
 import { IFileServer } from "coffee-workflow-analyzer-editors/lib/common/request-file-protocol";
+import { WorkspaceStorageServiceFilesystem } from "coffee-workspace-filesystem-storage-service/lib/browser/workspace-storage-service-filesystem";
 
 export const ANALYZE_COMMAND: Command = {
     id: "workflow.analyze.command",
@@ -22,7 +23,8 @@ export class WorkflowCommandContribution implements CommandContribution, MenuCon
         @inject(EditorManager) protected readonly editorManager: EditorManager,
         @inject(SelectionService) protected readonly selectionService: SelectionService,
         @inject(MiniBrowserOpenHandler) private readonly openHandler: MiniBrowserOpenHandler,
-        @inject(IFileServer) private readonly fileServer: IFileServer
+        @inject(IFileServer) private readonly fileServer: IFileServer,
+        @inject(WorkspaceStorageServiceFilesystem) private readonly workpaceFilesystemService: WorkspaceStorageServiceFilesystem
     ) { }
 
     registerMenus(menus: MenuModelRegistry): void {
@@ -34,39 +36,41 @@ export class WorkflowCommandContribution implements CommandContribution, MenuCon
     registerCommands(registry: CommandRegistry): void {
         registry.registerCommand(ANALYZE_COMMAND, this.newUriAwareCommandHandler({
             execute: async (uri: URI) => {
-                //const documentURL = uri.path.toString().replace("file://", "");
-                const xhttp = new XMLHttpRequest();
-                xhttp.open("POST", "http://localhost:9090/services/backend/wfanalysis", true);
-                xhttp.setRequestHeader("Content-type", "application/json");
-                xhttp.setRequestHeader("Accept", "application/json");
-                xhttp.send(JSON.stringify(load));
+                const configFilePath = uri.path.toString();
+                const configFileContent = this.workpaceFilesystemService.readFileContent(new URI(configFilePath));
 
-                xhttp.onreadystatechange = async (e) => {
-                    const htmlFile = await this.fileServer.requestFile(FileTypes.WORKFLOW_ANALYSIS_HTML);
-                    const jsonFile = escape(xhttp.responseText);
-                    const urlWithQuery = htmlFile + "?json=" + jsonFile;
-                    return await this.openHandler.open(undefined, { name: "Workflow Analysis", startPage: urlWithQuery, toolbar: 'hide' });
-                }
+                const wfFilePath = uri.path.toString().replace(".wfconfig", ".wf");
+                const wfFileContent = this.workpaceFilesystemService.readFileContent(new URI(wfFilePath));
+
+
+                Promise.all([configFileContent, wfFileContent]).then((result) => {
+                    const configContent = result[0];
+                    const wfContent = result[1];
+                    const content = {
+                        'graph': JSON.parse(wfContent),
+                        'config': configContent
+                    };
+                    const xhttp = new XMLHttpRequest();
+                    xhttp.open("POST", "http://localhost:9090/services/backend/wfanalysis", true);
+                    xhttp.setRequestHeader("Content-type", "application/json");
+                    xhttp.setRequestHeader("Accept", "application/json");
+                    xhttp.send(JSON.stringify(content));
+
+                    xhttp.onreadystatechange = async (e) => {
+                        const htmlFile = await this.fileServer.requestFile(FileTypes.WORKFLOW_ANALYSIS_HTML);
+                        const jsonFile = escape(xhttp.responseText);
+                        const urlWithQuery = htmlFile + "?json=" + jsonFile;
+                        return await this.openHandler.open(undefined, { name: "Workflow Analysis", startPage: urlWithQuery, toolbar: 'hide' });
+                    }
+
+                });
+
+
             },
             isVisible: (uri: URI) => uri.toString().endsWith("wfconfig"),
             isEnabled: (uri: URI) => uri.toString().endsWith("wfconfig")
         }),
         );
-
-        const load = {
-            graph: { "revision": 0, "type": "graph", "id": "sprotty", "children": [{ "name": "Push", "expanded": false, "duration": 30, "taskType": "manual", "layout": "vbox", "position": { "x": 10.0, "y": 200.0 }, "type": "node:task", "id": "task1", "children": [{ "layout": "hbox", "type": "comp:header", "id": "task1_header", "children": [{ "layout": "stack", "position": { "x": 0.0, "y": 0.0 }, "layoutOptions": { "resizeContainer": false, "hAlign": "center" }, "type": "icon", "id": "task1_icon", "children": [{ "text": "M", "type": "label:icon", "id": "task1_ticon" }] }, { "text": "Push", "type": "label:heading", "id": "task1_classname" }] }] }, { "name": "ChkWt", "expanded": false, "duration": 10, "taskType": "automated", "reference": "ControlUnit", "layout": "vbox", "position": { "x": 200.0, "y": 200.0 }, "type": "node:task", "id": "task2", "children": [{ "layout": "hbox", "type": "comp:header", "id": "task2_header", "children": [{ "layout": "stack", "position": { "x": 0.0, "y": 0.0 }, "layoutOptions": { "resizeContainer": false, "hAlign": "center" }, "type": "icon", "id": "task2_icon", "children": [{ "text": "A", "type": "label:icon", "id": "task2_ticon" }] }, { "text": "ChkWt", "type": "label:heading", "id": "task2_classname" }] }] }, { "name": "RflWt", "expanded": false, "duration": 200, "taskType": "manual", "layout": "vbox", "position": { "x": 400.0, "y": 150.0 }, "type": "node:task", "id": "task3", "children": [{ "layout": "hbox", "type": "comp:header", "id": "task3_header", "children": [{ "layout": "stack", "position": { "x": 0.0, "y": 0.0 }, "layoutOptions": { "resizeContainer": false, "hAlign": "center" }, "type": "icon", "id": "task3_icon", "children": [{ "text": "M", "type": "label:icon", "id": "task3_ticon" }] }, { "text": "RflWt", "type": "label:heading", "id": "task3_classname" }] }] }, { "name": "WtOK", "expanded": false, "duration": 20, "taskType": "automated", "reference": "WaterTank", "layout": "vbox", "position": { "x": 400.0, "y": 250.0 }, "type": "node:task", "id": "task4", "children": [{ "layout": "hbox", "type": "comp:header", "id": "task4_header", "children": [{ "layout": "stack", "position": { "x": 0.0, "y": 0.0 }, "layoutOptions": { "resizeContainer": false, "hAlign": "center" }, "type": "icon", "id": "task4_icon", "children": [{ "text": "A", "type": "label:icon", "id": "task4_ticon" }] }, { "text": "WtOK", "type": "label:heading", "id": "task4_classname" }] }] }, { "name": "ChkTp", "expanded": false, "duration": 100, "taskType": "manual", "reference": "ControlUnit", "layout": "vbox", "position": { "x": 600.0, "y": 200.0 }, "type": "node:task", "id": "task5", "children": [{ "layout": "hbox", "type": "comp:header", "id": "task5_header", "children": [{ "layout": "stack", "position": { "x": 0.0, "y": 0.0 }, "layoutOptions": { "resizeContainer": false, "hAlign": "center" }, "type": "icon", "id": "task5_icon", "children": [{ "text": "M", "type": "label:icon", "id": "task5_ticon" }] }, { "text": "ChkTp", "type": "label:heading", "id": "task5_classname" }] }] }, { "name": "PreHeat", "expanded": false, "duration": 1000, "taskType": "manual", "reference": "BrewingUnit", "layout": "vbox", "position": { "x": 600.0, "y": 400.0 }, "type": "node:task", "id": "task6", "children": [{ "layout": "hbox", "type": "comp:header", "id": "task6_header", "children": [{ "layout": "stack", "position": { "x": 0.0, "y": 0.0 }, "layoutOptions": { "resizeContainer": false, "hAlign": "center" }, "type": "icon", "id": "task6_icon", "children": [{ "text": "M", "type": "label:icon", "id": "task6_ticon" }] }, { "text": "PreHeat", "type": "label:heading", "id": "task6_classname" }] }] }, { "name": "KeepTp", "expanded": false, "duration": 50, "taskType": "manual", "reference": "BrewingUnit", "layout": "vbox", "position": { "x": 600.0, "y": 500.0 }, "type": "node:task", "id": "task7", "children": [{ "layout": "hbox", "type": "comp:header", "id": "task7_header", "children": [{ "layout": "stack", "position": { "x": 0.0, "y": 0.0 }, "layoutOptions": { "resizeContainer": false, "hAlign": "center" }, "type": "icon", "id": "task7_icon", "children": [{ "text": "M", "type": "label:icon", "id": "task7_ticon" }] }, { "text": "KeepTp", "type": "label:heading", "id": "task7_classname" }] }] }, { "name": "Brew", "expanded": false, "duration": 1750, "taskType": "manual", "reference": "Brew", "layout": "vbox", "position": { "x": 400.0, "y": 450.0 }, "type": "node:task", "id": "task8", "children": [{ "layout": "hbox", "type": "comp:header", "id": "task8_header", "children": [{ "layout": "stack", "position": { "x": 0.0, "y": 0.0 }, "layoutOptions": { "resizeContainer": false, "hAlign": "center" }, "type": "icon", "id": "task8_icon", "children": [{ "text": "M", "type": "label:icon", "id": "task8_ticon" }] }, { "text": "Brew", "type": "label:heading", "id": "task8_classname" }] }] }, { "nodeType": "decisionNode", "position": { "x": 350.0, "y": 216.0 }, "type": "node:activity", "id": "activityNode9" }, { "nodeType": "decisionNode", "position": { "x": 650.0, "y": 316.0 }, "type": "node:activity", "id": "activityNode11" }, { "nodeType": "mergeNode", "position": { "x": 550.0, "y": 216.0 }, "type": "node:activity", "id": "activityNode10" }, { "nodeType": "mergeNode", "position": { "x": 550.0, "y": 466.0 }, "type": "node:activity", "id": "activityNode12" }, { "sourceId": "task1", "targetId": "task2", "type": "edge", "id": "edge1" }, { "sourceId": "task2", "targetId": "activityNode9", "type": "edge", "id": "edge2" }, { "probability": "high", "sourceId": "activityNode9", "targetId": "task3", "type": "edge:weighted", "id": "edge3" }, { "probability": "high", "sourceId": "activityNode9", "targetId": "task4", "type": "edge:weighted", "id": "edge4" }, { "sourceId": "task3", "targetId": "activityNode10", "type": "edge", "id": "edge5" }, { "sourceId": "task4", "targetId": "activityNode10", "type": "edge", "id": "edge6" }, { "sourceId": "activityNode10", "targetId": "task5", "type": "edge", "id": "edge7" }, { "sourceId": "task5", "targetId": "activityNode11", "type": "edge", "id": "edge8" }, { "probability": "high", "sourceId": "activityNode11", "targetId": "task6", "type": "edge:weighted", "id": "edge9" }, { "probability": "low", "sourceId": "activityNode11", "targetId": "task7", "type": "edge:weighted", "id": "edge10" }, { "sourceId": "task6", "targetId": "activityNode12", "type": "edge", "id": "edge11" }, { "sourceId": "task7", "targetId": "activityNode12", "type": "edge", "id": "edge12" }, { "sourceId": "activityNode12", "targetId": "task8", "type": "edge", "id": "edge13" }] },
-            config: `workflowModel : sc3000
-
-            probabilities
-            low    : 0.25
-            medium : 0.5
-            high   : 0.75
-            
-            assertions
-            ChkWt => Brew,b
-            ChkTp => Brew
-            `
-        }
     }
 
     protected newUriAwareCommandHandler(handler: UriCommandHandler<URI>): UriAwareCommandHandler<URI> {
