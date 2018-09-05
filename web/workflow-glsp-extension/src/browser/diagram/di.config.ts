b91c438c78dc4984170652a05bc9e528474715fa
Tobias Ortmayr <tobias.ortmayr@gmail.com>
2018-09-05 11:37:58 +0200

    Updates workflow-example packages to conform to latest glsp release (#77)
    * Updates workflow-example packages to conform to latest glsp release
    
    * Fix anchoring on rotated decision nodes

--- web/workflow-glsp-extension/src/browser/diagram/di.config.ts
+++ web/workflow-glsp-extension/src/browser/diagram/di.config.ts
@@ -8,7 +8,7 @@
  * Contributors:
  * 	Tobias Ortmayr - initial API and implementation
  ******************************************************************************/
-import { DiagramConfiguration, TheiaKeyTool } from "theia-glsp/lib"
+import { DiagramConfiguration, TheiaKeyTool, SprottySelectionForwardingInitializer } from "theia-glsp/lib"
 import { createWorkflowDiagramContainer } from "workflow-sprotty/lib"
 import { TYPES, KeyTool } from "glsp-sprotty/lib"
 import { Container, injectable } from "inversify";
@@ -23,6 +23,7 @@ export class WorkflowDiagramConfiguration implements DiagramConfiguration {
         const container = createWorkflowDiagramContainer(widgetId);
         container.bind(TYPES.ModelSource).to(GLSPTheiaDiagramServer)
         container.rebind(KeyTool).to(TheiaKeyTool).inSingletonScope()
+        container.bind(TYPES.IActionHandlerInitializer).to(SprottySelectionForwardingInitializer)
         return container;
     }
 
