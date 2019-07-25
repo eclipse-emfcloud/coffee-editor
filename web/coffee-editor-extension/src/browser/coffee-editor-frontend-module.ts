c3520580e248528093ea2e68783f94d7e9fc6737
Eugen Neufeld <eneufeld@eclipsesource.com>
2019-07-25 12:51:49 +0200

    Fix js build

--- web/coffee-editor-extension/src/browser/coffee-editor-frontend-module.ts
+++ web/coffee-editor-extension/src/browser/coffee-editor-frontend-module.ts
@@ -1,16 +1,16 @@
 import "../../src/browser/style/index.css";
 import {
-  OpenHandler,
-  WidgetFactory,
-  NavigatableWidgetOptions,
+  // OpenHandler,
+  // WidgetFactory,
+  // NavigatableWidgetOptions,
   LabelProviderContribution
 } from "@theia/core/lib/browser";
-import URI from "@theia/core/lib/common/uri";
+// import URI from "@theia/core/lib/common/uri";
 import { ContainerModule } from "inversify";
-import { CoffeeTreeEditorContribution } from "./coffee-editor-tree-contribution";
+// import { CoffeeTreeEditorContribution } from "./coffee-editor-tree-contribution";
 import {
   JsonFormsTreeEditorWidget,
-  JsonFormsTreeEditorWidgetOptions
+//   JsonFormsTreeEditorWidgetOptions
 } from "./json-forms-tree-editor/json-forms-tree-editor-widget";
 import { createJsonFormsTreeWidget } from "./json-forms-tree/json-forms-tree-container";
 import { JsonFormsTreeWidget } from "./json-forms-tree/json-forms-tree-widget";
@@ -20,25 +20,25 @@ export default new ContainerModule(bind => {
   bind(JsonFormsTreeWidget).toDynamicValue(context =>
     createJsonFormsTreeWidget(context.container)
   );
-  bind(OpenHandler).to(CoffeeTreeEditorContribution);
+  // bind(OpenHandler).to(CoffeeTreeEditorContribution);
   bind(JsonFormsTreeEditorWidget).toSelf();
 
-  bind<WidgetFactory>(WidgetFactory).toDynamicValue(context => ({
-    id: JsonFormsTreeEditorWidget.WIDGET_ID,
-    createWidget: (options: NavigatableWidgetOptions) => {
-      const { container } = context;
-      const child = container.createChild();
+  // bind<WidgetFactory>(WidgetFactory).toDynamicValue(context => ({
+  //   id: JsonFormsTreeEditorWidget.WIDGET_ID,
+  //   createWidget: (options: NavigatableWidgetOptions) => {
+  //     const { container } = context;
+  //     const child = container.createChild();
 
-      const uri = new URI(options.uri);
-      child
-        .bind<JsonFormsTreeEditorWidgetOptions>(
-          JsonFormsTreeEditorWidgetOptions
-        )
-        .toConstantValue({
-          uri: uri
-        });
+  //     const uri = new URI(options.uri);
+  //     child
+  //       .bind<JsonFormsTreeEditorWidgetOptions>(
+  //         JsonFormsTreeEditorWidgetOptions
+  //       )
+  //       .toConstantValue({
+  //         uri: uri
+  //       });
 
-      return child.get(JsonFormsTreeEditorWidget);
-    }
-  }));
+  //     return child.get(JsonFormsTreeEditorWidget);
+  //   }
+  // }));
 });
