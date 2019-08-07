38e82c2b178a68be7046c2462c9fbfd496cea8e0
Eugen Neufeld <eneufeld@eclipsesource.com>
2019-08-07 23:31:36 +0200

    Remove unneeded projects

--- web/coffee-workspace-filesystem-storage-service/src/browser/frontend-extension.ts
+++
@@ -1,6 +0,0 @@
-import { ContainerModule } from "inversify";
-import { WorkspaceStorageServiceFilesystem } from "./workspace-storage-service-filesystem";
-
-export default new ContainerModule(bind => {
-    bind(WorkspaceStorageServiceFilesystem).toSelf().inSingletonScope();
-});
