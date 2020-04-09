8d789d67a553ee36cae240cce7aae976be1974fc
Jonas Helming <jhelming@eclipsesource.com>
2020-04-09 13:06:50 +0200

    Create a welcome page #238Signed-off-by: Jonas Helming <jhelming@eclipsesource.com>Signed-off-by: Jonas Helming <jhelming@eclipsesource.com>Signed-off-by: Jonas Helming <jhelming@eclipsesource.com>Signed-off-by: Jonas Helming <jhelming@eclipsesource.com>Signed-off-by: Jonas Helming <jhelming@eclipsesource.com>
    Signed-off-by: Jonas Helming <jhelming@eclipsesource.com>

---
+++ web/coffee-welcome-page/src/browser/coffee-welcome-page-contribution.ts
@@ -0,0 +1,66 @@
+/*!
+ * Copyright (C) 2020 EclipseSource and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+
+import { injectable, inject } from 'inversify';
+import { WelcomePageWidget } from './welcome-page-widget';
+import { CommandRegistry, MenuModelRegistry } from '@theia/core/lib/common';
+import { CommonMenus, AbstractViewContribution, FrontendApplicationContribution, FrontendApplication } from '@theia/core/lib/browser';
+import { FrontendApplicationStateService } from '@theia/core/lib/browser/frontend-application-state';
+import { WorkspaceService } from '@theia/workspace/lib/browser';
+
+export const WelcomePageCommand = {
+    id: WelcomePageWidget.ID,
+    label: WelcomePageWidget.LABEL
+};
+
+@injectable()
+export class CoffeeWelcomePageContribution extends AbstractViewContribution<WelcomePageWidget> implements FrontendApplicationContribution {
+    @inject(FrontendApplicationStateService)
+    protected readonly stateService: FrontendApplicationStateService;
+
+    @inject(WorkspaceService)
+    protected readonly workspaceService: WorkspaceService;
+
+    constructor() {
+        super({
+            widgetId: WelcomePageWidget.ID,
+            widgetName: WelcomePageWidget.LABEL,
+            defaultWidgetOptions: {
+                area: 'main',
+            }
+        });
+    }
+
+    async onStart(app: FrontendApplication): Promise<void> {
+        this.stateService.reachedState('ready').then(
+            a => this.openView({ reveal: true })
+        );
+    }
+
+    registerCommands(registry: CommandRegistry): void {
+        registry.registerCommand(WelcomePageCommand, {
+            execute: () => this.openView({ reveal: true }),
+        });
+    }
+
+    registerMenus(menus: MenuModelRegistry): void {
+        menus.registerMenuAction(CommonMenus.HELP, {
+            commandId: WelcomePageCommand.id,
+            label: WelcomePageCommand.label,
+            order: 'a10'
+        });
+    }
+}
