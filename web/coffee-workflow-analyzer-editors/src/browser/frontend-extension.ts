8aab51baf5750e887d3828270d8afc6966e586c3
Eugen Neufeld <eneufeld@eclipsesource.com>
2020-04-10 12:52:55 +0200

    Remove light theme setting
    By removing the light theme setting the default dark theme is used.

--- web/coffee-workflow-analyzer-editors/src/browser/frontend-extension.ts
+++ web/coffee-workflow-analyzer-editors/src/browser/frontend-extension.ts
@@ -13,16 +13,12 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
-import { ThemeService } from '@theia/core/lib/browser/theming';
 import { LanguageClientContribution } from '@theia/languages/lib/browser';
 import { ContainerModule } from 'inversify';
 
 import { WorkflowClientContribution } from './language-contribution';
 
-const LIGHT_THEME_ID = 'light';
 export default new ContainerModule(bind => {
-    ThemeService.get().setCurrentTheme(LIGHT_THEME_ID);
-
     bind(WorkflowClientContribution).toSelf().inSingletonScope();
     bind(LanguageClientContribution).toDynamicValue(ctx => ctx.container.get(WorkflowClientContribution));
 });
