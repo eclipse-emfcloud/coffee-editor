71955099a517bb1b8528aa9d6907aa512dc78a9d
Martin Fleck <mfleck@eclipsesource.com>
2020-04-27 19:07:30 +0200

    Start workflow LSP early and separately from language contribution
    - Start workflow LSP separately from language contribution
    - Start workflow LSP as early as possible as backend contribution
    
    Signed-off-by: Martin Fleck <mfleck@eclipsesource.com>

--- web/coffee-workflow-analyzer-editors/src/node/language-contribution.ts
+++ web/coffee-workflow-analyzer-editors/src/node/language-contribution.ts
@@ -15,12 +15,8 @@
  */
 import { ILogger } from '@theia/core';
 import { BaseLanguageServerContribution, IConnection } from '@theia/languages/lib/node';
-import { ProcessErrorEvent } from '@theia/process/lib/node/process';
-import { spawn } from 'child_process';
-import * as glob from 'glob';
 import { inject, injectable } from 'inversify';
 import * as net from 'net';
-import * as path from 'path';
 import { createSocketConnection } from 'vscode-ws-jsonrpc/lib/server';
 
 function getPort(): number | undefined {
@@ -49,37 +45,11 @@ export class WorkflowContribution extends BaseLanguageServerContribution {
     readonly id = 'wfconfig';
     readonly name = 'WFCONFIG';
     serverConnection: IConnection | undefined = undefined;
-    serverStarted = false;
 
     start(clientConnection: IConnection): void {
         this.logger.info('[WorkflowDSL] Start Server for Client Connection.');
         const socketPort = getPort();
         if (socketPort) {
-            if (!this.serverStarted) {
-                const command = 'java';
-
-                const serverPath = path.resolve(__dirname, '..', '..', 'server');
-                const jarPaths = glob.sync('**/plugins/org.eclipse.equinox.launcher_*.jar', { cwd: serverPath });
-                if (jarPaths.length === 0) {
-                    throw new Error('[WorkflowDSL] Server launcher not found.');
-                }
-                const jarPath = path.resolve(serverPath, jarPaths[0]);
-                const args: string[] = [];
-                args.push(
-                    '-jar', jarPath
-                );
-
-                this.logger.info('[WorkflowDSL] Spawn Server Process from ' + jarPath);
-                const child = spawn(command, args, {
-                    detached: true,
-                    shell: true,
-                    stdio: 'inherit'
-                });
-                child.unref();
-
-                this.serverStarted = true;
-            }
-
             const socket = new net.Socket();
             this.logger.info('[WorkflowDSL] Create Socket Connection at ' + socketPort);
             this.serverConnection = createSocketConnection(socket, socket, () => {
@@ -93,11 +63,6 @@ export class WorkflowContribution extends BaseLanguageServerContribution {
         } else {
             this.logger.error('[WorkflowDSL] Unable to connect to Workflow Graphical Language Server: No Socket Port');
         }
-    }
-
-    protected onDidFailSpawnProcess(error: ProcessErrorEvent): void {
-        super.onDidFailSpawnProcess(error);
-        this.logger.error('[WorkflowDSL] Error starting Workflow language server', error);
     }
 
 }
---
+++ web/coffee-workflow-analyzer-editors/src/node/workflow-lsp-launcher.ts
@@ -0,0 +1,85 @@
+/********************************************************************************
+ * Copyright (c) 2020 EclipseSource and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+import { ILogger } from '@theia/core';
+import { BackendApplicationContribution } from '@theia/core/lib/node';
+import { ProcessErrorEvent } from '@theia/process/lib/node/process';
+import { RawProcess, RawProcessFactory } from '@theia/process/lib/node/raw-process';
+import * as cp from 'child_process';
+import * as glob from 'glob';
+import { inject, injectable } from 'inversify';
+import * as path from 'path';
+
+@injectable()
+export class WorkflowLSPServerLauncher implements BackendApplicationContribution {
+    @inject(RawProcessFactory) protected readonly processFactory: RawProcessFactory;
+    @inject(ILogger) private readonly logger: ILogger;
+
+    initialize() {
+        const command = 'java';
+
+        const serverPath = path.resolve(__dirname, '..', '..', 'server');
+        const jarPaths = glob.sync('**/plugins/org.eclipse.equinox.launcher_*.jar', { cwd: serverPath });
+        if (jarPaths.length === 0) {
+            throw new Error('[WorkflowDSL] Server launcher not found.');
+        }
+        const jarPath = path.resolve(serverPath, jarPaths[0]);
+        const args: string[] = ['-jar', jarPath];
+
+        this.logger.info('[WorkflowDSL] Spawn Server Process from ' + jarPath);
+        this.spawnProcessAsync(command, args, {
+            detached: true,
+            shell: true,
+            stdio: ['inherit', 'pipe']
+        });
+    }
+
+    protected spawnProcessAsync(command: string, args?: string[], options?: cp.SpawnOptions): Promise<RawProcess> {
+        const rawProcess = this.processFactory({ command, args, options });
+        rawProcess.errorStream.on('data', this.logError.bind(this));
+        rawProcess.outputStream.on('data', this.logInfo.bind(this));
+        return new Promise<RawProcess>((resolve, reject) => {
+            rawProcess.onError((error: ProcessErrorEvent) => {
+                this.onDidFailSpawnProcess(error);
+                if (error.code === 'ENOENT') {
+                    const guess = command.split(/\s+/).shift();
+                    if (guess) {
+                        reject(new Error(`Failed to spawn ${guess}\nPerhaps it is not on the PATH.`));
+                        return;
+                    }
+                }
+                reject(error);
+            });
+            process.nextTick(() => resolve(rawProcess));
+        });
+    }
+
+    protected onDidFailSpawnProcess(error: Error | ProcessErrorEvent): void {
+        this.logError(error.message);
+    }
+
+    protected logError(data: string | Buffer) {
+        if (data) {
+            this.logger.error(`ModelServerBackendContribution: ${data}`);
+        }
+    }
+
+    protected logInfo(data: string | Buffer) {
+        if (data) {
+            this.logger.info(`ModelServerBackendContribution: ${data}`);
+        }
+    }
+
+}
