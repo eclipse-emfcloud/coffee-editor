b1e1b9f91cb049a0d10d023cabda5e1793354032
Eugen Neufeld <eneufeld@eclipsesource.com>
2020-10-16 13:04:29 +0200

    Update Header and cleanup
    Set correct headers to all files.

--- web/coffee-editor-extension/src/browser/coffee-tree/coffee-tree-label-provider-contribution.ts
+++ web/coffee-editor-extension/src/browser/coffee-tree/coffee-tree-label-provider-contribution.ts
@@ -1,17 +1,12 @@
 /*!
- * Copyright (C) 2019 EclipseSource and others.
+ * Copyright (c) 2019-2020 EclipseSource and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0.
+ * http://www.eclipse.org/legal/epl-2.0, or the MIT License which is
+ * available at https://opensource.org/licenses/MIT.
  *
- * This Source Code may also be made available under the following Secondary
- * Licenses when the conditions for such availability set forth in the Eclipse
- * Public License v. 2.0 are satisfied: GNU General Public License, version 2
- * with the GNU Classpath Exception which is available at
- * https://www.gnu.org/software/classpath/license.html.
- *
- * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ * SPDX-License-Identifier: EPL-2.0 OR MIT
  */
 import { TreeEditor } from '@eclipse-emfcloud/theia-tree-editor';
 import { LabelProviderContribution } from '@theia/core/lib/browser';
@@ -19,7 +14,7 @@ import URI from '@theia/core/lib/common/uri';
 import { injectable } from 'inversify';
 
 import { CoffeeModel } from './coffee-model';
-import { CoffeeTreeEditorWidget } from './coffee-tree-editor-widget';
+import { CoffeeTreeEditorConstants } from './coffee-tree-editor-widget';
 
 const DEFAULT_COLOR = 'black';
 
@@ -54,7 +49,7 @@ export class CoffeeTreeLabelProvider implements LabelProviderContribution {
 
     public canHandle(element: object): number {
         if ((TreeEditor.Node.is(element) || TreeEditor.CommandIconInfo.is(element))
-            && element.editorId === CoffeeTreeEditorWidget.EDITOR_ID) {
+            && element.editorId === CoffeeTreeEditorConstants.EDITOR_ID) {
             return 1000;
         }
         return 0;
