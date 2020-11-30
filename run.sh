829e1f84272a748819f293ade382186326674d70
Eugen Neufeld <eneufeld@eclipsesource.com>
2020-11-30 18:37:41 +0100

    Use default browser instead of firefox beeing hardcoded
    Instead of using 'firefox', 'x-www-browser' is used.
    Thus the default browser is used instead.
    
    Fix #203

--- run.sh
+++ run.sh
@@ -132,7 +132,7 @@ fi
 
 if [ "$runFrontend" == "true" ]; then
   workspace=$(pwd)
-  (sleep 5 && firefox 127.1:3000/#/${workspace:1}/backend/examples/SuperBrewer3000)&
+  (sleep 5 && x-www-browser http://127.1:3000/#/${workspace:1}/backend/examples/SuperBrewer3000)&
   cd web/browser-app
   yarn start --hostname 0.0.0.0
 fi
