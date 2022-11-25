4e50f67806da7df2d61df53b7c946c367dc73f4d
Eugen Neufeld <eneufeld@eclipsesource.com>
2022-11-25 10:27:05 +0100

    Revert "Update to Java17"
    This reverts commit 86e4b2e14c70340942e06c2f73d9fd0e7d110f12.
    
    Reverting as there are issues with the xtext dsl that need to be
    fixed first.

--- backend/plugins/org.eclipse.emfcloud.coffee.workflow.dsl/src-gen/org/eclipse/emfcloud/coffee/workflow/dsl/serializer/WorkflowSemanticSequencer.java
+++ backend/plugins/org.eclipse.emfcloud.coffee.workflow.dsl/src-gen/org/eclipse/emfcloud/coffee/workflow/dsl/serializer/WorkflowSemanticSequencer.java
@@ -56,13 +56,11 @@ public class WorkflowSemanticSequencer extends AbstractDelegatingSemanticSequenc
 	}
 	
 	/**
-	 * <pre>
 	 * Contexts:
 	 *     Assertion returns Assertion
 	 *
 	 * Constraint:
 	 *     (before=STRING after=STRING)
-	 * </pre>
 	 */
 	protected void sequence_Assertion(ISerializationContext context, Assertion semanticObject) {
 		if (errorAcceptor != null) {
@@ -79,13 +77,11 @@ public class WorkflowSemanticSequencer extends AbstractDelegatingSemanticSequenc
 	
 	
 	/**
-	 * <pre>
 	 * Contexts:
 	 *     ProbabilityConfiguration returns ProbabilityConfiguration
 	 *
 	 * Constraint:
 	 *     (low=Float medium=Float high=Float)
-	 * </pre>
 	 */
 	protected void sequence_ProbabilityConfiguration(ISerializationContext context, ProbabilityConfiguration semanticObject) {
 		if (errorAcceptor != null) {
@@ -105,13 +101,11 @@ public class WorkflowSemanticSequencer extends AbstractDelegatingSemanticSequenc
 	
 	
 	/**
-	 * <pre>
 	 * Contexts:
 	 *     WorkflowConfiguration returns WorkflowConfiguration
 	 *
 	 * Constraint:
 	 *     (machine=STRING model=STRING probConf=ProbabilityConfiguration? (assertions+=Assertion assertions+=Assertion*)?)
-	 * </pre>
 	 */
 	protected void sequence_WorkflowConfiguration(ISerializationContext context, WorkflowConfiguration semanticObject) {
 		genericSequencer.createSequence(context, semanticObject);
